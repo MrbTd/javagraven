a9023ffd54d21159b57734582d3384a3966a84e7
Ben TOURE <ben.toure@ats.local>
2023-10-17 19:40:50 +0200

    handle widget dynamic

--- app/src/main/java/com/btd/myapplication/MainActivity.java
+++ app/src/main/java/com/btd/myapplication/MainActivity.java
@@ -2,29 +2,48 @@ package com.btd.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
+import android.app.ActionBar;
 import android.os.Bundle;
-import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView play;
-    @SuppressLint("MissingInflatedId")
+    private LinearLayout myLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity2_main);
+        setContentView(R.layout.activity3_main);
 
-       this.play= (ImageView) findViewById(R.id.play);
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent otherActivity=new Intent(getApplicationContext(),CookieActivity.class);
-                startActivity(otherActivity);
-                finish();
-            }
-        });
+        this.myLayout=(LinearLayout)  findViewById(R.id.myDynamicLayout);
+
+        ImageView image= new ImageView(this);
+
+        ViewGroup.LayoutParams params=new ActionBar.LayoutParams(100,100);
+        image.setLayoutParams(params);
+        image.setBackgroundResource(R.drawable.logo);
+        myLayout.addView(image);
+
+        TextView text= new TextView(this);
+
+        ActionBar.LayoutParams params2=new ActionBar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        text.setLayoutParams(params2);
+        text.setText(getResources().getString(R.string.welcome_mag));
+        text.setTextColor(getResources().getColor(R.color.red));
+        text.setTextSize(25);
+        myLayout.addView(text);
+        int textsize=10;
+        for(int i=0;i<6;i++){
+            Button btn= new Button(this);
+            btn.setText("Button"+i);
+            btn.setTextSize(textsize);
+            textsize+=5;
+        myLayout.addView(btn);
+
+        }
+
     }
 }
